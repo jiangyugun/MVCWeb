c8fae436f114003cc16fbb58ea5ec490b6847dca
jiangyugun <47242466+jiangyugun@users.noreply.github.com>
2022-02-20 17:31:43 +0800

    1.更新驗證規則至Model。 2.Edit.cshtml加入Rating欄位編輯

--- MVCWeb/Models/Movie.cs
+++ MVCWeb/Models/Movie.cs
@@ -8,16 +8,29 @@ namespace MVCWeb.Models
     {
         public int ID { get; set; }
 
+        [StringLength(60, MinimumLength = 3)]
+        [Display(Name = "電影名稱")]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
+        [Display(Name = "發布日期")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [Required]
+        [StringLength(30)]
+        [Display(Name = "電影類型")]
         public string Genre { get; set; }
 
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Display(Name = "價錢")]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z'\s]*$")]
+        [StringLength(5)]
+        [Display(Name = "評分")]
         public string Rating { get; set; }
     }
 
