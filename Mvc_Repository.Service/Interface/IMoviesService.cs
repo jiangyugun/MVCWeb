d05f65f8927e3fd323674f6b8d3b9f51f7544845
jiangyugun <47242466+jiangyugun@users.noreply.github.com>
2022-02-21 18:01:39 +0800

    建立 Service 層 (尚未完成)

---
+++ Mvc_Repository.Service/Interface/IMoviesService.cs
@@ -0,0 +1,24 @@
+﻿using MVCWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mvc_Repository.Service.Interface
+{
+    public interface IMoviesService
+    {
+        IResult Create(Movie instance);
+
+        IResult Update(Movie instance);
+
+        IResult Delete(int ID);
+
+        bool IsExists(int ID);
+
+        Movie GetByID(int categoryID);
+
+        IEnumerable<Movie> GetAll();
+    }
+}
